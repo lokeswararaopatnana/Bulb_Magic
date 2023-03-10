8ab64e7eafb53aba33e6b077615f0ee0e7cf60a7
PATNANA LOKESWARA RAO <109509575+Lokeshpatnana@users.noreply.github.com>
2023-03-10 12:41:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function on(){
+    document.getElementById('bulb').src = 'https://www.w3schools.com/js/pic_bulbon.gif'
+    alert('Light on!')
+}
+function off(){
+    document.getElementById('bulb').src = 'https://www.w3schools.com/js/pic_bulboff.gif'
+    alert('Are you sure want to turn off light?')
+}
